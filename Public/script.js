e5c1c100eaf33ca5ec9411bcd4044f00636226d0
Akpan Nnamnso <nnamnso.akpan@student.aul.edu.ng>
2024-11-16 12:10:59 +0100

    first commit

---
+++ Public/script.js
@@ -0,0 +1,6 @@
+const menuButton = document.getElementById('mobile-menu-button');
+const mobileMenu = document.getElementById('mobile-menu');
+
+menuButton.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+});
